6ac9283587c5a11b25eba2bd13ce59a82c43265d
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-12-01 11:55:29 -0600

    Sets are lot faster than Lists for part2. Doh!

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -5,13 +5,13 @@ object Day01 {
   def part1(data: List[Int]): Int = data.sum
 
   def part2(data: List[Int]): Int = {
-    def helper(f: Int, fs: List[Int], input: Stream[Int]): Int = {
+    def helper(f: Int, fs: Set[Int], input: Stream[Int]): Int = {
       val nextFreq = f + input.head
       if(fs.contains(nextFreq)) nextFreq
-      else                      helper(nextFreq, nextFreq +: fs, input.tail)
+      else                      helper(nextFreq, fs + nextFreq , input.tail)
     }
 
-    helper(0, List(), Stream.continually(data.toStream).flatten)
+    helper(0, Set(), Stream.continually(data.toStream).flatten)
   }
 
   def readFile(file: String): List[Int] = {
