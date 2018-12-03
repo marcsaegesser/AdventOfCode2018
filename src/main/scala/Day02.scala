1a21dd604768aa03e3b736304850d1373a8f22a1
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-12-02 19:40:02 -0600

    Add some comments.

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -2,6 +2,11 @@ package advent
 
 object Day02 {
 
+  /** For each string collect all the occurences of each letter (i.e. Map[Char, List[Char]]).
+    * The size of each list is the number of occurences of that character. We don't care about
+    * specific characters just the counts so extract just the values from Map and convert the
+    * result to Set to remove duplicates.
+    */
   def part1(input: List[String]): Int = {
     def countOccurences(s: String): Set[Int] = s.groupBy(identity).mapValues(_.size).values.toSet
     def filterByCount(s: Set[Int], n: Int): Boolean = s.contains(n)
@@ -13,6 +18,9 @@ object Day02 {
     twoCount * threeCount
   }
 
+  /** Given a pair of Strings find their longest common prefix. Return that prefix
+    * and the remaining tail of each string.
+    */
   def commonPrefix(pair: (String, String)): (String, (String, String)) = {
     def helper(p: List[Char], s1: List[Char], s2: List[Char]): (String, (String, String)) =
       (s1, s2) match {
@@ -23,13 +31,20 @@ object Day02 {
     helper(List(), pair._1.toList, pair._2.toList)
   }
 
-  def part2(input: List[String]): Iterator[String] = {
+  /** If two strings in the input list differ by only a single character
+    * then they will sort to be adjacent to each other. After sorting
+    * the input list we only need to compare adjacent elements for a
+    * match. This can be done easily using List.sliding(2, 1) to
+    * create a list of pairs of adjacent elements. After that we just
+    * traverse the list of pairs looking for any that match our
+    * constraints.
+    */
+  def part2(input: List[String]): Iterator[String] =
     input.sorted.sliding(2, 1).collect { case a :: b :: Nil => (a, b) }                   // Sort the input and create pairs of adjacent items
       .map(commonPrefix)                                                                  // Extract the common prefix from each pair and both remaining tails
       .collect { case (p, (t1, t2)) if t1.size > 0 && t2.size > 0  && t1.tail == t2.tail =>  // Collect the pairs with identical, non-empty tails
         p + t1.tail
       }
-  }
 
   def readFile(file: String): List[String] = {
     io.Source.fromFile(file)
