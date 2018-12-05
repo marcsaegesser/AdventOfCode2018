e5910bfabd414d881d4b4f5444dc71b37b47470c
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-12-05 07:51:01 -0600

    Day 5

---
+++ src/main/scala/Day05.scala
@@ -0,0 +1,38 @@
+package advent
+
+object Day05 {
+
+  def part1(input: List[Char]): Int =
+    runReaction(input).size
+
+  def part2(input: List[Char]): Int = {
+    ('a' to 'z')
+      .map(c => runReaction(filterUnits(c, input)).size)
+      .min
+  }
+
+  def filterUnits(c: Char, input: List[Char]): List[Char] =
+    input.filterNot(u => u.toLower == c.toLower)
+
+  def willReact(a: Char, b: Char): Boolean =
+    a.toLower == b.toLower && a != b
+
+  def reactor(left: List[Char], right: List[Char]): String =
+    (left, right) match {
+      case (l, Nil)             => l.reverse.mkString
+      case (Nil, h :: t)        => reactor(h :: Nil, t)
+      case (lh :: lt, rh :: rt) =>
+        if(willReact(lh, rh)) reactor(lt, rt)
+        else                  reactor(rh :: left, rt)
+    }
+
+  def runReaction(input: List[Char]): String =
+    reactor(List.empty[Char], input)
+
+  def loadData(file: String): List[Char] =
+    io.Source.fromFile(file)
+      .getLines()
+      .filterNot(_.isEmpty)
+      .toList
+      .flatten
+}
