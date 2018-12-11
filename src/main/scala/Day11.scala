3dfa5fd843b673a756ede4d1487604f416ef83d9
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-12-11 10:08:16 -0600

    Checkpoint.

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -7,26 +7,53 @@ object Day11 {
 
   type Grid = Map[Coord, Int]
 
-  def part1(serialNumber: Int): (Coord, Int) = {
+  def part1(serialNumber: Int): (Coord, Int, Int) =
+    maxForSize(createGrid(serialNumber), 3)
+
+  def part2(serialNumber: Int): (Coord, Int, Int) = {
     val grid = createGrid(serialNumber)
-    val positions = for { y <- 1 to 297; x <- 1 to 297 } yield Coord(x, y)
-    positions.foldLeft((Coord(0, 0), Int.MinValue)) { case ((m, p), c) =>
-      val pwr = powerForRegion(grid, c)
-      if(pwr > p) (c, pwr)
-      else        (m, p)
+    val sizes = (1 to 300)
+
+    sizes.map { s => maxForSize(grid, s) }.sortBy(_._2).last
+  }
+
+  def maxForSize(grid: Grid, size: Int): (Coord, Int, Int) = {
+    val positions = positionsForSize(size)
+    positions.foldLeft((Coord(0, 0), Int.MinValue, size)) { case ((m, p, _), c) =>
+      val pwr = powerForRegion(grid, c, size)
+      if(pwr > p) (c, pwr, size)
+      else        (m, p, size)
     }
   }
 
-  def powerForRegion(grid: Grid, c: Coord): Int = {
-    grid(c) +
-    grid(c + Coord(1, 0)) +
-    grid(c + Coord(2, 0)) +
-    grid(c + Coord(0, 1)) +
-    grid(c + Coord(1, 1)) +
-    grid(c + Coord(2, 1)) +
-    grid(c + Coord(0, 2)) +
-    grid(c + Coord(1, 2)) +
-    grid(c + Coord(2, 2))
+  def positionsForSize(size: Int) =
+    for {
+      y <- 1 to 300-size
+      x <- 1 to 300-size
+    } yield Coord(x, y)
+
+  // val cacheOffsetsForSize = collection.mutable.Map.empty[Int, IndexedSeq[Coord]]
+  // def offsetsForSize(size: Int): IndexedSeq[Coord] = {
+  //   cacheOffsetsForSize.get(size).getOrElse {
+  //     val coords =
+  //       for {
+  //         x <- 0 until size
+  //         y <- 0 until size
+  //       } yield Coord(x, y)
+
+  //     cacheOffsetsForSize(size) = coords
+  //     coords
+  //   }
+  // }
+
+  val offsetsForSize = {
+    (1 to 300).map { s =>
+      (s, for { x <- 0 until s; y <- 0 until s } yield Coord(x, y))
+    }.toMap
+  }
+
+  def powerForRegion(grid: Grid, c: Coord, size: Int): Int = {
+    offsetsForSize(size).foldLeft(0) { case (a, o) =>  a + grid(c + o)}
   }
 
   def createGrid(serialNum: Int): Grid = {
